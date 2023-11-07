3676603a2916517fc319690d7956faddefacc2b1
jerome_Hsieh <jerome910810@gmail.com>
2023-11-08 03:36:21 +0800

    Initial Commit

---
+++ swift_learn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift_learn
+//
+//  Created by HsinYuan on 2023/11/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift_learn/swift_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learnApp.swift
+//  swift_learn
+//
+//  Created by HsinYuan on 2023/11/8.
+//
+
+import SwiftUI
+
+@main
+struct swift_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
