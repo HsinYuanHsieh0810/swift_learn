966f3c7af536cadb81b1a81f50da1753ff5e8ebd
jerome_Hsieh <jerome910810@gmail.com>
2023-11-09 23:50:47 +0800

    2023.11.09

---
+++ swift_learn/Models/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  swift_learn
+//
+//  Created by HsinYuan on 2023/11/8.
+//
+
+import Foundation
---
+++ swift_learn/View/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  swift_learn
+//
+//  Created by HsinYuan on 2023/11/8.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
