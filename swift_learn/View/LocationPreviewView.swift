f42daae69429167087bae0686ac8a55e131da86d
jerome_Hsieh <jerome910810@gmail.com>
2023-11-13 18:33:26 +0800

    2023.11.13

---
+++ swift_learn/View/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  swift_learn
+//
+//  Created by HsinYuan on 2023/11/13.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
