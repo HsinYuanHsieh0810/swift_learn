4e0018564ecf91c73d6df3b8d4c60fe3cdf6800e
jerome_Hsieh <jerome910810@gmail.com>
2023-11-14 03:58:45 +0800

    2023.11.13 add map pin

---
+++ swift_learn/View/LocationMapAnnotionView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationMapAnnotionView.swift
+//  swift_learn
+//
+//  Created by HsinYuan on 2023/11/14.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationMapAnnotionView()
+}
